c3ab576d29b9d58c6480147e71b947098bfbf57d
Dawidu <dawidtomalik7@gmail.com>
2025-05-31 19:59:57 +0200

    Added Navbar

---
+++ frontend/src/app/_components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="py-4 text-center">
+      <h2 className="text-lg font-semibold">stability.ai</h2>
+      <p className="text-xs font-extralight">DIGITAL PRODUCT PASSPORT</p>
+    </nav>
+  )
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Main from "./_components/Main"
+import Navbar from "./_components/Navbar"
 import { type Metadata } from "next"
 import "~/styles/globals.css"
 import { TRPCReactProvider } from "~/trpc/react"
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: Children) {
     <html lang="en">
       <body className="bg-bg text-white">
         <TRPCReactProvider>
+          <Navbar />
           <Main>{children}</Main>
         </TRPCReactProvider>
       </body>
