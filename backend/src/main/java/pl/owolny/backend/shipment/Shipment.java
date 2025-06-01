986964266a12c1d510795e28ad5e1342a72b4f6e
Vetther <osk4r.wolny@gmail.com>
2025-06-01 09:03:22 +0200

    Updated Shipment class

--- backend/src/main/java/pl/owolny/backend/shipment/Shipment.java
+++ backend/src/main/java/pl/owolny/backend/shipment/Shipment.java
@@ -2,7 +2,10 @@ package pl.owolny.backend.shipment;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import pl.owolny.backend.product.Product;
+import pl.owolny.backend.product.vo.ProductId;
 import pl.owolny.backend.shipment.vo.ShipmentId;
 import pl.owolny.backend.shipment.vo.ShipmentStatus;
 
@@ -30,28 +33,32 @@ public class Shipment {
     @Enumerated(EnumType.STRING)
     private ShipmentStatus type;
 
-    @OneToMany(mappedBy = "shipment", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ShipmentStage> stages = new ArrayList<>(); // Lista etapów przesyłki
+    @Column(columnDefinition = "jsonb")
+    @JdbcTypeCode(SqlTypes.JSON)
+    private List<ShipmentStage> shipmentStages = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
+    private ProductId productId;
 
-    public Shipment(String countryFrom, String countryFromCode, String countryTo, String countryToCode, ShipmentStatus type, Product product) {
+    public Shipment(String countryFrom, String countryFromCode, String countryTo, String countryToCode,
+                    ShipmentStatus type, ProductId productId) {
         this.id = ShipmentId.generate();
         this.countryFrom = countryFrom;
         this.countryFromCode = countryFromCode;
         this.countryTo = countryTo;
         this.countryToCode = countryToCode;
         this.type = type;
-        this.product = product;
+        this.productId = productId;
     }
 
     public Shipment() {
     }
 
     public void addStage(ShipmentStage stage) {
-        stages.add(stage);
+        this.shipmentStages.add(stage);
+    }
+
+    public void removeStage(ShipmentStage stage) {
+        this.shipmentStages.remove(stage);
     }
 
 }
