f04eed1680036f3fb8f62a19db60f43ca56f20dc
rejman <janswidzinskiprograming@gmail.com>
2025-06-01 10:29:52 +0200

    Created smart contract

--- blockchain/data_base/programs/data_base/src/lib.rs
+++ blockchain/data_base/programs/data_base/src/lib.rs
@@ -6,11 +6,24 @@ declare_id!("83aMjCUBPTMLcJKZ9KUowaHREGrm5vMXbjPPW3arWUM4");
 pub mod data_base {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, message: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.message = message;
+        msg!("Saved message!");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 4 + 280)] // 8 bytes for discriminator, 4 for string length prefix, 280 for max string
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub message: String,
+}
