a24c6c9ad4949876b3c693f27bdce57a7a9bd92d
Vetther <osk4r.wolny@gmail.com>
2025-06-01 11:11:22 +0200

    ShipmentStage rework

--- backend/src/main/java/pl/owolny/backend/shipment/ShipmentStage.java
+++ backend/src/main/java/pl/owolny/backend/shipment/ShipmentStage.java
@@ -1,30 +1,19 @@
 package pl.owolny.backend.shipment;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Embeddable;
 import lombok.Getter;
-import pl.owolny.backend.shipment.vo.ShipmentStageId;
 
-import java.time.LocalDateTime;
-
-@Entity
 @Getter
 public class ShipmentStage {
 
-    @EmbeddedId
-    @AttributeOverride(name = "value", column = @Column(name = "id"))
-    private ShipmentStageId id;
-
     private String name; // np. "Produkcja", "Transport", "Magazyn"
     private String description; // np. "Produkcja baterii", "Transport baterii z Francji do Polski"
-    private LocalDateTime dateTime; // Data i czas etapu
 
     public ShipmentStage() {
     }
 
-    public ShipmentStage(String name, String description, LocalDateTime dateTime) {
-        this.id = ShipmentStageId.generate();
+    public ShipmentStage(String name, String description) {
         this.name = name;
         this.description = description;
-        this.dateTime = dateTime;
     }
 }
